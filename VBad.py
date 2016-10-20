f8399e6f85fbd2dcd8bfab9299e0df8f70f9306d
Florent <Pepitoh@users.noreply.github.com>
2016-10-20 15:23:04 +0200

    Implementation of Fake keys (+ cool banner)
    Implementation of fake keys using + cool banner

--- VBad.py
+++ VBad.py
@@ -34,6 +34,18 @@ def file_len(fname):
     return i + 1
 
 def main():
+
+    print '''
+    __     ______            _ 
+    \ \   / / __ )  __ _  __| |
+     \ \ / /|  _ \ / _` |/ _` |
+      \ V / | |_) | (_| | (_| |
+       \_/  |____/ \__,_|\__,_|
+       
+            VBA Obfuscation Tools combined with an MS office document generator
+            By @Pepitoh
+    '''
+    
     file_type = return_file_type(template_file)
 
     filenames = open_file(filename_list, "r")
@@ -68,18 +80,37 @@ def main():
 
         Office_container.Open(template_file)
         VBA_Func = VBA_Functions()
+        
         #Adding keys :
         if key_hiding_method == "doc_variable":
-            Info("Using Document.Variables method for hiding ciphering keys",0,2)
-            Office_container.AddVba(VBA_Func.generate_generic_store_function("ActivateKey", vba.key_name, vba.key), "tmp")
-            Office_container.RunMacro("ActivateKey")
-            Office_container.DeleteVbaModule("tmp")
+            if add_fake_keys:
+                #The keys are stored in the doc and are sorted alphabetically with their name
+                #Small keys are stored at the beggining (using a-v as first letter) in order to hide the location of the real key in the doc
+                #Long keys (containing real key) are stored in random places (using Random names)
+                Info("Adding " +str(small_keys)+ " fake small keys before real ones",0,2)
+                for z in range(small_keys):
+                    random_var_name_first = random_value(2, string.ascii_letters)
+                    Office_container.AddVba(VBA_Func.generate_generic_store_function("Activate_fake_Key_before", random_value(1, 'abcdfeghijklmnoprstuv') + random_value(random.randint(3,10), string.ascii_letters), random_value(random.randint(3,10), string.ascii_letters)), random_var_name_first)
+                    Office_container.RunMacro("Activate_fake_Key_before")
+                    Office_container.DeleteVbaModule(random_var_name_first)    
 
+                Info("Adding "+str(big_keys)+" fake big keys",0,2)
+                for k in range(big_keys):
+                    random_vba_name = random_value(1, string.ascii_letters)
+                    Office_container.AddVba(VBA_Func.generate_generic_store_function("Activate_fake_Key_after", random_value(1, 'abcdfeghijklmnoprstuv') +  random_value(random.randint(3,10), string.ascii_letters), random_value(encryption_key_length - random.randint(0,100))), random_vba_name)
+                    Office_container.RunMacro("Activate_fake_Key_after")
+                    Office_container.DeleteVbaModule(random_vba_name)         
+                    
+            Info("Using Document.Variables method for hiding ciphering keys (real ones)",0,2)
+            Office_container.AddVba(VBA_Func.generate_generic_store_function("ActivateKey", vba.key_name, vba.key), "k")
+            Office_container.RunMacro("ActivateKey")
+            Office_container.DeleteVbaModule("k")
+
+            Info("onClose auto-action was chosen, add trick to bypass first closing of the document : ",0,2)
             if auto_function_macro == "onClose":
-                Info("onClose auto-action was chosen, add trick to bypass first closing of the document : ",0,2)
-                Office_container.AddVba(VBA_Func.generate_generic_store_function("OncloseKey", trigger_close_test_name, trigger_close_test_value), "tmp2")
+                Office_container.AddVba(VBA_Func.generate_generic_store_function("OncloseKey", trigger_close_test_name, trigger_close_test_value), "tmp5")
                 Office_container.RunMacro("OncloseKey")
-                Office_container.DeleteVbaModule("tmp2")
+                Office_container.DeleteVbaModule("tmp5")
                 #Wrapping function
 
             elif auto_function_macro == "onOpen":
